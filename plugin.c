f96f61730bcd5c4d0e58d7f72579efc8444230f4
toby <toby@d127708d-35f1-0310-882e-b6c776866be2>
2007-04-19 04:48:13 +0000

    changes for Win32 build
    git-svn-id: http://www.telegraphics.com.au/svn/psdparse/trunk@221 d127708d-35f1-0310-882e-b6c776866be2

--- plugin.c
+++ plugin.c
@@ -19,6 +19,10 @@
 
 #include "psdparse.h"
 
+#ifdef WIN32
+	#define fsread_large FSRead
+#endif
+
 int pl_fgetc(psd_file_t f){
 	// FIXME: replace with buffered version, this will be SLOW
 	unsigned char c;
@@ -58,4 +62,28 @@ int pl_feof(psd_file_t f){
 
 void pl_fatal(char *s){
 	// TODO
+}
+
+// stubs to keep linker happy.
+
+FILE* pngsetupwrite(psd_file_t psd, char *dir, char *name, psd_pixels_t width, psd_pixels_t height, 
+					int channels, int color_type, struct layer_info *li, struct psd_header *h)
+{
+	return NULL;
+}
+
+void pngwriteimage(FILE *png,psd_file_t psd, int comp[], struct layer_info *li, psd_bytes_t **rowpos,
+				   int startchan, int pngchan, psd_pixels_t rows, psd_pixels_t cols, struct psd_header *h)
+{
+}
+
+FILE* rawsetupwrite(psd_file_t psd, char *dir, char *name, psd_pixels_t width, psd_pixels_t height, 
+					int channels, int color_type, struct layer_info *li, struct psd_header *h)
+{
+	return NULL;
+}
+
+void rawwriteimage(FILE *png,psd_file_t psd, int comp[], struct layer_info *li, psd_bytes_t **rowpos,
+				   int startchan, int pngchan, psd_pixels_t rows, psd_pixels_t cols, struct psd_header *h)
+{
 }
