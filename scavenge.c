1d3f49877d1cd7c90c0e1e51abf6146cc8573b15
toby <toby@d127708d-35f1-0310-882e-b6c776866be2>
2009-05-15 03:32:40 +0000

    When scavenging to find the layer/mask info block, locate and skip over image resources first.
    git-svn-id: http://www.telegraphics.com.au/svn/psdparse/trunk@264 d127708d-35f1-0310-882e-b6c776866be2

--- scavenge.c
+++ scavenge.c
@@ -95,7 +95,7 @@ unsigned scan(unsigned char *addr, size_t len, int psb_flag, struct layer_info *
 										++li;
 									}
 									else
-										VERBOSE("@ %8d : key: %c%c%c%c  could be %d channels: t = %ld, l = %ld, b = %ld, r = %ld\n",
+										VERBOSE("@ %8d : key: %c%c%c%c  could be %d channel layer: t = %ld, l = %ld, b = %ld, r = %ld\n",
 											   q - p - 16,
 											   de->key[0], de->key[1], de->key[2], de->key[3],
 											   j,
@@ -114,16 +114,51 @@ unsigned scan(unsigned char *addr, size_t len, int psb_flag, struct layer_info *
 	return n;
 }
 
+int is_resource(unsigned char *addr, size_t len, size_t offset)
+{
+	int namelen;
+	long size;
+
+	if(!memcmp("8BIM", addr + offset, 4)){
+		offset += 4; // type
+		offset += 2; // id
+		namelen = addr[offset];
+		offset += PAD2(1+namelen);
+		size = peek4B(addr+offset);
+		offset += 4;
+		offset += PAD2(size); // skip resource block data
+		return offset; // should be offset of next resource
+	}
+	return 0;
+}
+
 void scan_merged(unsigned char *addr, size_t len, int psb_flag, struct psd_header *h)
 {
-	size_t i, ps_ptr_bytes = psb_flag ? 8 : 4;
+	size_t i, j = 0, ps_ptr_bytes = psb_flag ? 8 : 4;
 
 	// if no valid layer signature was found, then look for an empty layer/mask info block
 	// which would imply only merged data is in the file (no layers)
 
 	h->lmistart = h->lmilen = 0;
 
-	for(i = 0; i < len; ++i)
+	// first search for a valid image resource block (these precede layer/mask info)
+	for(i = 0; i < len;)
+	{
+		j = is_resource(addr, len, i);
+		if(j && j < len-4){
+			VERBOSE("possible resource id=%d @ %lu\n", peek2B(addr+i+4), i);
+			i = j; // found an apparently valid resource; skip over it
+			
+			// is it followed by another resource?
+			if(memcmp("8BIM", addr+j, 4))
+				break; // no - stop looking
+		}else
+			++i;
+	}
+
+	if(!j) i = 0; // saw no resources, start over
+
+	for(; i < len; ++i)
 	{
 		psd_bytes_t lmilen = psb_flag ? peek8B(addr+i) : peek4B(addr+i),
 				  layerlen = psb_flag ? peek8B(addr+i+ps_ptr_bytes) : peek4B(addr+i+ps_ptr_bytes);;
@@ -136,7 +171,7 @@ void scan_merged(unsigned char *addr, size_t len, int psb_flag, struct psd_heade
 				h->lmistart = i+ps_ptr_bytes;
 				h->lmilen = lmilen;
 				VERBOSE("possible empty LMI @ %lld\n", h->lmistart);
-				//break;
+				break; // take first one
 			}
 		}
 	}
@@ -164,7 +199,7 @@ int scavenge_psd(int fd, struct psd_header *h, int psb_flag,
 				if( (h->linfo = checkmalloc(h->nlayers*sizeof(struct layer_info))) )
 					scan(addr, sb.st_size, psb_flag, h->linfo);
 			}
-			else if(rows && cols)
+			else
 				scan_merged(addr, sb.st_size, psb_flag, h);
 
 			UNQUIET("possible layers (PS%c): %d\n", psb_flag ? 'B' : 'D', h->nlayers);
