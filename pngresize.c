834ef2a8f618531429fe92d502b73a7fff3dcff3
toby <toby@d127708d-35f1-0310-882e-b6c776866be2>
2011-08-13 01:08:47 +0000

    Cleanup.
    git-svn-id: http://www.telegraphics.com.au/svn/psdparse/trunk@450 d127708d-35f1-0310-882e-b6c776866be2

--- pngresize.c
+++ pngresize.c
@@ -21,9 +21,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <math.h>
-#include <sys/resource.h>
 #include <sys/errno.h>
 
 #include "png.h"
@@ -34,6 +32,7 @@
 
 #define HEADER_BYTES 8
 #define MB(x) ((x) << 20)
+#define OVERSAMPLE_RATE 1.0
 
 // This was very helpful in understanding Lanczos kernel and convolution:
 // http://stackoverflow.com/questions/943781/where-can-i-find-a-good-read-about-bicubic-interpolation-and-lanczos-resampling/946943#946943
@@ -48,8 +47,6 @@ int clamp(int vv){
 
 double sinc(double x){ return sin(x)/x; }
 
-#define OVERSAMPLE_RATE 2.0
-
 // build 1-D kernel
 // conv: array of 2n+1 floats (-n .. 0 .. +n)
 void lanczos_1d(double *conv, unsigned n, double fac){
@@ -59,7 +56,7 @@ void lanczos_1d(double *conv, unsigned n, double fac){
 	// at 1.0, there is a single non-zero coefficient.
 	conv[n] = 1.;
 	for(i = 1; i <= n; ++i){
-		double x = i*M_PI / (fac*OVERSAMPLE_RATE);
+		double x = i*M_PI / fac;
 		conv[n+i] = conv[n-i] = sinc(x)*sinc(x/n);
 	}
 	/*for(i = 0; i <= 2*n; ++i)
@@ -67,15 +64,15 @@ void lanczos_1d(double *conv, unsigned n, double fac){
 	putchar('\n');*/
 }
 
-void lanczos_decim( png_bytep *row_pointers,  // input image: array of row pointers
-					int in_planes,      // bytes separating input planes;
-					                    // allows for interleaved input
+void lanczos_decim( png_bytep *row_ptrs, // input image: array of row pointers
+					int in_planes,       // bytes separating input planes;
+					                     // allows for interleaved input
 					int plane_index,
-					int in_w,           // image columns
-					int in_h,           // image rows
-                    unsigned char *out, // pointer to output buffer (always one plane)
-                    int out_w,          // output columns
-                    int out_h,          // output rows
+					int in_w,            // image columns
+					int in_h,            // image rows
+                    unsigned char *out,  // pointer to output buffer (always one plane)
+                    int out_w,           // output columns
+                    int out_h,           // output rows
                     double scale )       // ratio of input to output (> 1.0)
 {
 	double *conv, *knl, sum, weight;
@@ -85,7 +82,7 @@ void lanczos_decim( png_bytep *row_pointers,  // input image: array of row point
 	
 	conv = malloc((2*KERNEL_SIZE+1)*sizeof(double));
 
-	lanczos_1d(conv, KERNEL_SIZE, scale);
+	lanczos_1d(conv, KERNEL_SIZE, scale*OVERSAMPLE_RATE);
 	knl = conv + KERNEL_SIZE; // zero index of kernel
 
 	// apply horizontal convolution, interpolating input to output
@@ -93,9 +90,8 @@ void lanczos_decim( png_bytep *row_pointers,  // input image: array of row point
 
 	t_buf = malloc(sizeof(*t_buf) * in_h * out_w);
 
-	for(j = 0, t_row = t_buf;  j < in_h;  ++j, t_row += out_w)
-	{
-		in_row = row_pointers[j] + plane_index;
+	for(j = 0, t_row = t_buf;  j < in_h;  ++j, t_row += out_w){
+		in_row = row_ptrs[j] + plane_index;
 		for(i = 0; i < out_w; ++i){
 			// find involved input pixels. multiply each by corresponding
 			// kernel coefficient.
@@ -140,18 +136,15 @@ void lanczos_decim( png_bytep *row_pointers,  // input image: array of row point
 }
 
 int main(int argc, char *argv[]){
-	const struct rlimit mem_limit = { MB(300), MB(300) },
-						cpu_limit = { 120, 120 };
-	FILE *fp;
-	char out_name[0x100];
-	unsigned char header[HEADER_BYTES];
+	static png_uint_32 new_width, new_height;
+	FILE *in_file, *out_file;
+	unsigned char header[HEADER_BYTES], out_name[0x100];
 	png_structp png_ptr, wpng_ptr;
 	png_infop info_ptr, end_info, winfo_ptr;
-	png_bytep *row_pointers;
-	png_uint_32 width, height, max_pixels, new_width, new_height, i, j;
-    int bit_depth, color_type, interlace_method, compression_method,
-        filter_method, channels, rowbytes,
-        larger_dimension, k;
+	png_bytep *row_ptrs, out_plane[4];
+	png_uint_32 width, height, max_pixels, i, j;
+    int bit_depth, color_type, interlace, compression,
+        filter, channels, rowbytes, larger_dimension, k;
 	
 	if(argc <= 3){
 		fprintf(stderr,
@@ -162,58 +155,41 @@ int main(int argc, char *argv[]){
 		return 2;
 	}
 	
-	max_pixels = atoi(argv[3]);
-	
-	setrlimit(RLIMIT_AS, &mem_limit);
-	setrlimit(RLIMIT_CPU, &cpu_limit);
-	
-    fp = fopen(argv[1], "rb");
-    if (!fp){
-        fputs("# cannot open the file\n", stderr);
+    in_file = fopen(argv[1], "rb");
+    if (!in_file){
+        fputs("# cannot open input file\n", stderr);
         return EXIT_FAILURE;
     }
-    if (fread(header, 1, HEADER_BYTES, fp) < HEADER_BYTES
+
+    if (fread(header, 1, HEADER_BYTES, in_file) < HEADER_BYTES
         || png_sig_cmp(header, 0, HEADER_BYTES))
     {
         fputs("# not a PNG file\n", stderr);
         return EXIT_FAILURE;
     }
-    
-    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png_ptr){
-        fputs("# png_create_read_struct failed\n", stderr);
-        return EXIT_FAILURE;
-    }
 
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr){
-        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+    if (!(png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL))
+     || !(info_ptr = png_create_info_struct(png_ptr))
+     || !(end_info = png_create_info_struct(png_ptr)))
+    {
         fputs("# png_create_info_struct failed\n", stderr);
-        return EXIT_FAILURE;
-    }
-
-    end_info = png_create_info_struct(png_ptr);
-    if (!end_info){
-        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
-        fputs("# png_create_info_struct failed (end_info)\n", stderr);
         return EXIT_FAILURE;
     }
     
     if (setjmp(png_jmpbuf(png_ptr))){
-        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
         fputs("# libpng failed to read the file\n", stderr);
         return EXIT_FAILURE;
     }
 
-    png_init_io(png_ptr, fp);
+    png_init_io(png_ptr, in_file);
     png_set_sig_bytes(png_ptr, HEADER_BYTES);
 
     png_read_info(png_ptr, info_ptr);
     png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth,
-    			 &color_type, &interlace_method, &compression_method, &filter_method);
-
+    			 &color_type, &interlace, &compression, &filter);
 
 	larger_dimension = width > height ? width : height;
+	max_pixels = atoi(argv[3]);
 	new_width = max_pixels*width/larger_dimension;
 	new_height = max_pixels*height/larger_dimension;
     printf("new width: %lu  new height: %lu\n", new_width, new_height);
@@ -236,17 +212,14 @@ int main(int argc, char *argv[]){
 		sprintf(out_name, "%s_%lu_%lu.png", argv[2], new_width, new_height);
 	}
 
-	// scale image and write new png
-
-	if(interlace_method != PNG_INTERLACE_NONE){
+	if(interlace != PNG_INTERLACE_NONE){
         fputs("# interlace not supported\n", stderr);
         return EXIT_FAILURE;
 	}
-	
 
     printf("file channels: %u  file rowbytes: %lu\n",
-    	   png_get_channels(png_ptr, info_ptr),
-    	   png_get_rowbytes(png_ptr, info_ptr));
+    png_get_channels(png_ptr, info_ptr),
+    png_get_rowbytes(png_ptr, info_ptr));
 
 	// define transformations
     if (color_type == PNG_COLOR_TYPE_PALETTE)
@@ -262,44 +235,37 @@ int main(int argc, char *argv[]){
     printf("transformed channels: %d  rowbytes: %d\n", channels, rowbytes);
         
 // -------------- read source image --------------
-   row_pointers = calloc(height, sizeof(png_bytep));
+   row_ptrs = calloc(height, sizeof(png_bytep));
    for(i = 0; i < height; ++i)
-      row_pointers[i] = malloc(width*channels);
-   png_read_image(png_ptr, row_pointers);
+      row_ptrs[i] = malloc(width*channels);
+   png_read_image(png_ptr, row_ptrs);
 
 // -------------- filter image --------------
-   unsigned char *out_plane[4];
    for(k = 0; k < channels; ++k){
-		out_plane[k] = malloc(new_width*new_height);
-	   lanczos_decim(row_pointers, channels, k /* plane index */, width, height,
+	   out_plane[k] = malloc(new_width*new_height);
+	   lanczos_decim(row_ptrs, channels, k /* plane index */, width, height,
 	                 out_plane[k], new_width, new_height,
 	                 (double)larger_dimension/max_pixels);
    }
 	
 // -------------- write output image --------------
-   FILE *out_file = fopen(out_name, "wb");
-
-    wpng_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!wpng_ptr)
-       return EXIT_FAILURE;
-
-    winfo_ptr = png_create_info_struct(wpng_ptr);
-    if (!winfo_ptr){
-       png_destroy_write_struct(&wpng_ptr, (png_infopp)NULL);
+    if (!(out_file = fopen(out_name, "wb"))
+     || !(wpng_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL))
+     || !(winfo_ptr = png_create_info_struct(wpng_ptr)))
+    {
        return EXIT_FAILURE;
     }
 
     if (setjmp(png_jmpbuf(wpng_ptr))){
-        png_destroy_write_struct(&wpng_ptr, &winfo_ptr);
         fputs("# libpng failed to write the file\n", stderr);
         return EXIT_FAILURE;
     }
     
     png_init_io(wpng_ptr, out_file);
-    static int coltype[] = {0, PNG_COLOR_TYPE_GRAY, PNG_COLOR_TYPE_GRAY_ALPHA, PNG_COLOR_TYPE_RGB, PNG_COLOR_TYPE_RGB_ALPHA};
-    png_set_IHDR(wpng_ptr, winfo_ptr, new_width, new_height,
-       8, coltype[channels],
-       PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    static int coltype[] = {0, PNG_COLOR_TYPE_GRAY, PNG_COLOR_TYPE_GRAY_ALPHA,
+							PNG_COLOR_TYPE_RGB, PNG_COLOR_TYPE_RGB_ALPHA};
+    png_set_IHDR(wpng_ptr, winfo_ptr, new_width, new_height, 8, coltype[channels],
+				 PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 
     png_write_info(wpng_ptr, winfo_ptr);
     
